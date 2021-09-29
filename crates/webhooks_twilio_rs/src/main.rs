db17108a1ed52aaf6340b2f1c586ad5a74cadb51
Aydrian Howard <aydrian@gmail.com>
2021-09-29 16:45:44 -0400

    removed twilio package, basic webhook in rust

--- crates/webhooks_twilio_rs/src/main.rs
+++ crates/webhooks_twilio_rs/src/main.rs
@@ -3,41 +3,19 @@ use lamedh_http::{
     lambda::{lambda, Context, Error},
     IntoResponse, Request, Response,
 };
-use twilio::twiml::{Message, Twiml};
-//use twilio::TwilioError;
 
 #[lambda(http)]
 #[tokio::main]
 async fn main(_request: Request, _: Context) -> Result<impl IntoResponse, Error> {
-    /*let request = request.map(|body| body.as_ref().into());
-    let account_sid = std::env::var("TWILIO_ACCOUNT_SID").expect("TWILIO_ACCOUNT_SID was not set");
-    let auth_token = std::env::var("TWILIO_AUTH_TOKEN").expect("TWILIO_AUTH_TOKEN was not set");
-    let client = twilio::Client::new(&account_sid, &auth_token);
-
-    match client.parse_request::<twilio::Message>(request).await.err() {
-        Some(TwilioError::AuthError) => {
-            return Ok(Response::builder()
-                .status(StatusCode::UNPROCESSABLE_ENTITY)
-                .body(String::from("Signature verification failed."))
-                .unwrap());
-        }
-        Some(TwilioError::BadRequest) => {
-            return Ok(Response::builder()
-                .status(StatusCode::BAD_REQUEST)
-                .body(String::from("Bad Request"))
-                .unwrap());
-        }
-        _ => {}
-    }*/
-
-    let mut t = Twiml::new();
-    t.add(&Message {
-        txt: String::from("Response from rust"),
-    });
+    // TODO: Verify signature
+    let twiml = format!(
+        "<Response><Message><Body>{}</Body><Message></Response>",
+        "Response from rust"
+    );
 
     Ok(Response::builder()
         .status(StatusCode::OK)
         .header("Content-Type", "text/xml")
-        .body(t.as_twiml())
+        .body(twiml)
         .unwrap())
 }
