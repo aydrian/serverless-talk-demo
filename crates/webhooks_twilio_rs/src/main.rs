7c3161c4bc6fbc8bfbcb423647fde4b7156b5153
Aydrian Howard <aydrian@gmail.com>
2021-09-29 13:38:52 -0400

    base twilio webhook functionality

--- crates/webhooks_twilio_rs/src/main.rs
+++ crates/webhooks_twilio_rs/src/main.rs
@@ -3,12 +3,41 @@ use lamedh_http::{
     lambda::{lambda, Context, Error},
     IntoResponse, Request, Response,
 };
+use twilio::twiml::{Message, Twiml};
+//use twilio::TwilioError;
 
 #[lambda(http)]
 #[tokio::main]
-async fn main(_: Request, _: Context) -> Result<impl IntoResponse, Error> {
+async fn main(_request: Request, _: Context) -> Result<impl IntoResponse, Error> {
+    /*let request = request.map(|body| body.as_ref().into());
+    let account_sid = std::env::var("TWILIO_ACCOUNT_SID").expect("TWILIO_ACCOUNT_SID was not set");
+    let auth_token = std::env::var("TWILIO_AUTH_TOKEN").expect("TWILIO_AUTH_TOKEN was not set");
+    let client = twilio::Client::new(&account_sid, &auth_token);
+
+    match client.parse_request::<twilio::Message>(request).await.err() {
+        Some(TwilioError::AuthError) => {
+            return Ok(Response::builder()
+                .status(StatusCode::UNPROCESSABLE_ENTITY)
+                .body(String::from("Signature verification failed."))
+                .unwrap());
+        }
+        Some(TwilioError::BadRequest) => {
+            return Ok(Response::builder()
+                .status(StatusCode::BAD_REQUEST)
+                .body(String::from("Bad Request"))
+                .unwrap());
+        }
+        _ => {}
+    }*/
+
+    let mut t = Twiml::new();
+    t.add(&Message {
+        txt: String::from("Response from rust"),
+    });
+
     Ok(Response::builder()
         .status(StatusCode::OK)
-        .body(String::from("OK"))
+        .header("Content-Type", "text/xml")
+        .body(t.as_twiml())
         .unwrap())
 }
--- netlify/functions/webhooks_twilio_js.js
+++ netlify/functions/webhooks_twilio_js.js
@@ -1,6 +1,39 @@
-export async function handler(event, context) {
+import Twilio from "twilio";
+
+const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
+
+export async function handler(event, _context) {
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 405,
+      headers: { Allow: "POST" },
+      body: "Method Not Allowed"
+    };
+  }
+
+  const twilioSignature = event.headers["X-Twilio-Signature"];
+  if (
+    !Twilio.validateRequestWithBody(
+      twilioAuthToken,
+      twilioSignature,
+      "https://serverless-talk-demo.netlify.app/webhooks/twilio",
+      event.body
+    )
+  ) {
+    return {
+      statusCode: 422,
+      body: "Signature verification failed."
+    };
+  }
+
+  const twiml = new Twilio.twiml.MessagingResponse();
+  twiml.message("Response from javascript");
+
   return {
     statusCode: 200,
-    body: "OK"
+    headers: {
+      "Content-Type": "text/xml"
+    },
+    body: twiml.toString()
   };
 }
