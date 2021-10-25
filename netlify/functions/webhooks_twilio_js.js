403dfd613c6159f688ad1829860cea20a7f2c4a3
Aydrian Howard <aydrian@gmail.com>
2021-10-25 10:37:14 -0400

    Added db insert to javascript function

--- netlify/functions/webhooks_twilio_js.js
+++ netlify/functions/webhooks_twilio_js.js
@@ -1,12 +1,37 @@
 const Twilio = require("twilio");
+const { Client } = require("pg");
+const format = require("pg-format");
+const fs = require("fs");
+const path = require("path");
 
 const { withVerifyTwilio } = require("../lib/withVerifyTwilio");
+
+const client = new Client({
+  ssl: {
+    ca: fs.readFileSync(path.join(__dirname, "../certs/cc-ca.crt")).toString()
+  }
+});
 
 async function twilioHandler(event, _context) {
   const { parsedBody } = event;
   console.log(parsedBody);
 
-  const username = parsedBody.Body.trim();
+  const { Body, FromCity, FromState, FromZip, FromCountry } = parsedBody;
+  const username = Body.trim();
+
+  try {
+    await client.connect();
+    const sqlStatement = format(
+      "INSERT INTO messages (github_username, sms_location, function_used) VALUES (%L, %L, 'JavaScript');",
+      username,
+      { FromCity, FromState, FromZip, FromCountry }
+    );
+    await client.query(sqlStatement);
+  } catch (ex) {
+    console.log("Error inserting message", ex);
+  } finally {
+    await client.end();
+  }
 
   const twiml = new Twilio.twiml.MessagingResponse();
   const message = twiml.message();
