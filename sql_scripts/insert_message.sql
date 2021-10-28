6b3c0624618321237252ad4e1ec8550a866a3da1
Aydrian Howard <aydrian@gmail.com>
2021-10-28 14:23:02 -0400

    use rust for twilio webhook

--- sql_scripts/insert_message.sql
+++ sql_scripts/insert_message.sql
@@ -1,1 +1,2 @@
-INSERT INTO messages (github_username, sms_location, function_used) VALUES ('aydrian', '{"FromCity":"INDIANAPOLIS", "FromState": "IN", "FromZip": "46222", "FromCountry": "US"}', 'JavaScript');
+INSERT INTO messages (github_username, sms_location, function_used) 
+VALUES ('aydrian', '{"FromCity":"INDIANAPOLIS", "FromState": "IN", "FromZip": "46222", "FromCountry": "US"}', 'JavaScript');
