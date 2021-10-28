feccf4e83531b88074b0299337f18478a9f84b58
Aydrian Howard <aydrian@gmail.com>
2021-10-28 13:46:44 -0400

    added database write to rust

--- crates/webhooks_twilio/src/main.rs
+++ crates/webhooks_twilio/src/main.rs
@@ -4,7 +4,9 @@ use crypto::sha1::Sha1;
 use http::StatusCode;
 use lambda_runtime::{handler_fn, Context, Error};
 use serde_json::{json, Value};
+use sqlx::postgres::PgPool;
 use std::collections::BTreeMap;
+use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -26,6 +28,19 @@ async fn handler(event: Value, _: Context) -> Result<Value, Error> {
     println!("parsed body: {:?}", parsed_body);
     let username = parsed_body.get("Body").unwrap().trim();
     println!("username: {}", username);
+    let from_city = "";
+    let from_state = "";
+    let from_zip = "";
+    let from_country = "";
+
+    let location = json!({
+        "FromCity":from_city, "FromState": from_state, "FromZip": from_zip, "FromCountry": from_country
+    });
+
+    if let Ok(ret) = add_message(username, location).await {
+        println!("Message added: {}", ret);
+    }
+
     let media_url = format!(
         "https://serverless-talk-demo.netlify.app/generate/image?username={}",
         username
@@ -67,4 +82,27 @@ fn parse_twilio_event(event: Value) -> (bool, BTreeMap<String, String>) {
     let result = hmac.result();
     let expected = MacResult::new(&base64::decode(twilio_signature.as_bytes()).unwrap());
     (result == expected, post_args)
+}
+
+async fn add_message(username: &str, location: Value) -> Result<String, Error> {
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL was not set.");
+    dbg!(&database_url);
+
+    let pool = PgPool::connect(&database_url).await?;
+
+    let rec = sqlx::query!(
+        r#"
+        INSERT INTO messages (github_username, sms_location, function_used) 
+        VALUES ($1, $2, $3)
+        RETURNING id
+        "#,
+        username,
+        location,
+        "Rust"
+    )
+    .fetch_one(&pool)
+    .await?;
+    dbg!(&rec);
+
+    Ok(rec.id.to_string())
 }
